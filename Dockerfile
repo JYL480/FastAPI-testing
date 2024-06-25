ecf92cef2867f321054558110cb6f79554d7dd7b
JYL480 <leejunyang480@gmail.com>
2024-06-25 11:05:34 +0800

    Added middleware for CORS

--- Dockerfile
+++ Dockerfile
@@ -5,12 +5,16 @@ WORKDIR /app
 
 # within the container
 
+RUN python3 -m venv venv
+ENV VIRTUAL_ENV=/app/venv
+ENV PATH="$VIRTUAL_ENV/bin:$PATH"
+
 COPY ./requirements.txt /app/requirements.txt
 
 RUN pip install --no-cache-dir --upgrade -r /app/requirements.txt
 
-EXPOSE 80
-
 COPY ./app /app
+
+EXPOSE 80
 
 CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "80"]
--- app/main.py
+++ app/main.py
@@ -7,8 +7,24 @@ from fastapi.responses import FileResponse
 import os
 import shutil
 from pathlib import Path
+from fastapi.middleware.cors import CORSMiddleware
 # Create a check first
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:8000"
+    "http://localhost:8081",
+    "http://localhost:8080"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 UPLOAD_DIRECTORY = "uploads"
 latest_img_path = ""
